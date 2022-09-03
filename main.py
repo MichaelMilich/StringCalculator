ab7245bd4332227fdd46835298ff27b41fa3e827
Michael Millich <michael.milich1@gmail.com>
2022-09-03 20:56:03 +0300

    I want to make the calculator work by always removing the last number and operator used untill the list of operators is empty. problem is I dont know yet how to do it when there are different operators with different mporance, some operators go before others '*' goes before '+' and '^' goes before that. need to find a way to deal with this problem.
    Currently the calculator only works with * by hard code, have to correct that.
    Also, the Idea of removing items is good, I will go with it for now only for '+' and '-' and the i will solve the rest.

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 # This is a sample Python script.
+import re
+
 
 # Press Shift+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
@@ -8,9 +10,39 @@ def print_hi(name):
     # Use a breakpoint in the code line below to debug your script.
     print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
 
+def check_letters(input):
+    return re.search("[a-zA-Z]",input)
+
+def turn_into_numbers_list(input):
+    return re.split("[*+-]",input)
+
+def turn_into_operators_list(input):
+    return re.findall("[*+-]",input)
+
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     print_hi('PyCharm')
+
+user_input = input("Welcome to calculator\n "
+                   "To escape please write 'esc' \n"
+                   "Please write what you want to calculate\n")
+
+while user_input!="esc":
+    if check_letters(user_input):
+        print("NO LETTERS!")
+    else:
+        print("Calculating...")
+        numbers= turn_into_numbers_list(user_input)
+        operators = turn_into_operators_list(user_input)
+        for idx, operator in enumerate(operators):
+            if operator=="*":
+                numbers[idx] = float(numbers[idx]) * float(numbers[idx+1])
+                del numbers[idx+1]
+                del operators[idx]
+        else:
+            print(numbers)
+    user_input=input("Please write what you want to calculate\n")
+print("DONE")
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
